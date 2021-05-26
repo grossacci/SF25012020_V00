ec3830fbac0a47af2a8e482db6e5855551e3452a
fg <fg@DESKTOP-I3EQ1DG>
2021-05-26 14:31:49 +0200

    26052021

--- MANUT_SOFTWARE/RDI/RDI_Service/Generali/RDI_ServiziUserControl.cs
+++ MANUT_SOFTWARE/RDI/RDI_Service/Generali/RDI_ServiziUserControl.cs
@@ -4,12 +4,10 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace MANUT_SOFTWARE.Servizi.RDI_Servizi
+namespace MANUT_SOFTWARE.RDI.RDI_Service.RDI_Servizi_UserControl
 {
-    class RDIServizi
+    class RDI_ServiziUserControl
     {
-
-
 
 
 
